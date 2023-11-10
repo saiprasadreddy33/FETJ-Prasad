df42fce3e5577e94a62666594b74ec58e1d5d496
Sai Prasad Reddy <74254400+saiprasadreddy33@users.noreply.github.com>
2023-11-10 07:53:25 +0530

    Update script.js

--- my-auth-app/public/script.js
+++ my-auth-app/public/script.js
@@ -2,30 +2,31 @@ document.addEventListener('DOMContentLoaded', function () {
   const displayButton = document.getElementById('displayButton');
   displayButton.addEventListener('click', () => {
     const numLines = parseInt(document.getElementById('numLines').value, 10);
-//alert('clicked');
+
     if (numLines >= 1 && numLines <= 100) {
       const designContainer = document.getElementById('designContainer');
-      //numLines.length
       designContainer.innerHTML = '';
 
       const word = 'FORMULAQSOLUTIONS';
       const wordLength = word.length;
-      const middle = Math.floor(numLines / 2);
-      //var flag = false;
-      for (let i = 0; i < numLines * 2 - 1; i++) {
-        const line = document.createElement('p');
-        let currentWordIndex = Math.abs(middle - i) % wordLength;
 
-        for (let j = 0; j < Math.abs(middle - i); j++) {
-         // flag = true;
-          line.innerText += '  ';
+      for (let i = 0; i < numLines; i++) {
+        const line = document.createElement('p');
+
+        for (let j = 0; j <= i; j++) {
+          line.innerText += word[j % wordLength];
         }
-       // alert('flag');
-        for (let j = 0; j < numLines * 2 - 1 - 2 * Math.abs(middle - i); j++) {
-          line.innerText += word[currentWordIndex];
-          currentWordIndex = (currentWordIndex + 1) % wordLength;
+
+        designContainer.appendChild(line);
+      }
+
+      for (let i = numLines - 2; i >= 0; i--) {
+        const line = document.createElement('p');
+
+        for (let j = 0; j <= i; j++) {
+          line.innerText += word[j % wordLength];
         }
-         console.log(currentWordIndex);
+
         designContainer.appendChild(line);
       }
     } else {
